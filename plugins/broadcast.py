1fda425366eb36e9fe5df2ee2e0e084448a8bb5b
kevinnadar <jesikamaraj@gmail.com>
2022-11-07 11:14:27 +0530

    added cancel comd

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -12,12 +12,23 @@ from utils import broadcast_messages
 
 broadcast_ids = {} 
 
+
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 async def verupikkals(bot, message: types.Message):
     try:
         await main_broadcast_handler(message)
     except Exception as e:
         print(e)
+
+@Client.on_message(filters.command("stop_broadcast") & filters.user(ADMINS))
+async def verupikkals(bot, message: types.Message):
+    cmd = message.command
+    if len(cmd) == 1:
+        return await message.reply("`/stop_broadcast broadcast_id`")
+    elif len(cmd) == 2:
+        broadcast_id = cmd[1]
+        await update_broadcast(broadcast_id=broadcast_id, value={"ongoing": False})
+        await message.reply("Broadcast has been Stopped")
 
 async def resume_broadcast(c: Client, broadcast_id):
     info = await get_broadcast_info(broadcast_id)
@@ -61,6 +72,7 @@ async def main_broadcast_handler(message: types.Message, offset: int = 0, new_b=
     if new_b:
         await new_broadcast(broadcast_id, total_users, b_msg.id, b_msg.chat.id)
 
+
     async for user in users:
         pti, sh = await broadcast_messages(int(user['id']), b_msg)
         if pti:
@@ -73,7 +85,12 @@ async def main_broadcast_handler(message: types.Message, offset: int = 0, new_b=
             failed += 1
         done += 1
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}") 
+            
+            info = await get_broadcast_info(broadcast_id)
+            if not info["ongoing"]:
+                break
+
+            await sts.edit(f"ID: `{broadcast_id}`\nBroadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}") 
             await update_broadcast(broadcast_id, {"total_users_done": done})
 
         if done >= total_users:
@@ -84,4 +101,4 @@ async def main_broadcast_handler(message: types.Message, offset: int = 0, new_b=
     except Exception as e:
         print(e)
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    await sts.edit(f"ID: `{broadcast_id}`\nBroadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
